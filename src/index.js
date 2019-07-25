e14404649a3f5979b4df39c1c63759e162f15090
Santiago Blankleider <santiago@btss-MacBook-Pro.local>
2019-07-24 21:35:50 -0300

    Initial structure, architecture, linters and modules

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Route, hashHistory } from "react-router";
+import App from "./components/App";
+import "bootstrap/dist/css/bootstrap.css";
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={App} />
+  </Router>,
+
+  document.getElementById("root")
+);
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+const outputDirectory = 'dist';
+
+module.exports = {
+  entry: ['babel-polyfill', './src/index.js'],
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|otf|svg)$/,
+        loader: 'url-loader'
+      },
+      {
+        test: /\.(mov|mp4)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+    ]
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    })
+  ]
+};
